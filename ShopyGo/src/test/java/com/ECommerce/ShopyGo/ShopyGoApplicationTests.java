6368bb31fd47fc77751ea49ffa61f2f12b18b3a0
roshin <roshinp4@gmail.com>
2023-12-07 02:42:44 +0530

    restapi for product first commit of the project

---
+++ ShopyGo/src/test/java/com/ECommerce/ShopyGo/ShopyGoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ECommerce.ShopyGo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopyGoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ elregalo/src/main/java/com/myproject/elregalo/ElregaloApplication.java
@@ -0,0 +1,13 @@
+package com.myproject.elregalo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElregaloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElregaloApplication.class, args);
+	}
+
+}
